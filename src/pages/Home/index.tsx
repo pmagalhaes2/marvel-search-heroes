963b5c13a3596caefa9146edcc1654ee6897b9a5
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-07-29 15:00:49 -0300

    feat: create home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,30 @@
+import { useCallback, useEffect, useState } from "react";
+import { ICharacterInfo, getAllCharacters } from "../../services/MarveAPI";
+
 export const Home = () => {
-  return <h1>Home</h1>
+  const [characters, setCharacters] = useState<ICharacterInfo[]>([]);
+
+  const fetchData = useCallback(() => {
+    getAllCharacters().then((response: ICharacterInfo[]) =>
+      setCharacters(response)
+    );
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <>
+      {characters.map((character) => (
+        <div key={character.id}>
+          <img
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt=""
+          />
+          <p>{character.name}</p>
+        </div>
+      ))}
+    </>
+  );
 };
